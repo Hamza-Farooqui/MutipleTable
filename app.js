9e2850107cbb69cd63d7c4a79b40ef0a25f6a0d7
Hamza farooqui <145295344+Hamza-Farooqui@users.noreply.github.com>
2024-03-01 07:35:55 -0800

    Just little bit

---
+++ app.js
@@ -0,0 +1,6 @@
+
+let userinp = prompt('Enter a table number')
+// let userinp1 = prompt('Enter a end number')
+for(let i = 1; i <=10; i++){
+    document.write(userinp  + " x "  + i + " = " + userinp*i + " <br> ")
+  }     
